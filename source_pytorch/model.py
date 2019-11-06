fb8cca052820ef2ac8752a2b31b864043e7f4b9f
EC2 Default User <ec2-user@ip-172-16-156-142.ec2.internal>
2019-11-06 01:54:49 +0000

    add arch of NN

--- source_pytorch/model.py
+++ source_pytorch/model.py
@@ -27,9 +27,12 @@ class BinaryClassifier(nn.Module):
         super(BinaryClassifier, self).__init__()
 
         # define any initial layers, here
+        self.fc1 = nn.Linear(input_features, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, output_dim)
+        self.drop = nn.Dropout(0.3)
+        # sigmoid layer
+        self.sig = nn.Sigmoid()
         
-
-    
     ## TODO: Define the feedforward behavior of the network
     def forward(self, x):
         """
@@ -39,6 +42,9 @@ class BinaryClassifier(nn.Module):
         """
         
         # define the feedforward behavior
+        out = F.relu(self.fc1(x))
+        out = self.drop(out)
+        out = self.fc2(out)
         
-        return x
+        return self.sig(out)
     
